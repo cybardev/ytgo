d983c48a04dc22258612a877a57ba56929a427b4
cybardev <sheikh@cybar.dev>
2024-02-27 12:27:09 -0400

    feat: allow audio-only playback

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"io"
 	"log"
 	"net/http"
@@ -11,20 +12,44 @@ import (
 )
 
 func main() {
-	playNth("gurenge band cover", 1)
+	// specify available flags
+	var query string
+	var n int
+	var m bool
+
+	// parse CLI args
+	flag.BoolVar(&m, "m", false, "Play music only")
+	flag.IntVar(&n, "n", 1, "Play nth media")
+	flag.Parse()
+
+	query = flag.Arg(0)
+	if query == "" {
+		log.Println(query)
+		log.Fatalln("No query provided")
+	}
+
+	// play media from YT
+	playNth(query, n, m)
 }
 
-func playNth(query string, n int) {
+func playNth(query string, n int, m bool) {
 	vids := getVideos(query)
-	if n > len(vids) {
+	if 0 > n || n > len(vids) {
 		log.Fatalln("No video found")
 	}
-	play(videoURL(vids[n-1]))
+	play(videoURL(vids[n-1]), m)
 }
 
-func play(url string) {
-	cmd := exec.Command("mpv", url)
-	cmd.Run()
+func play(url string, m bool) {
+	bestaudio, novideo := "", ""
+	if m {
+		bestaudio, novideo = "--ytdl-format=bestaudio", "--no-video"
+	}
+	cmd := exec.Command("mpv", bestaudio, novideo, url)
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func videoURL(id string) string {
@@ -61,7 +86,7 @@ func fetch(url string) string {
 		log.Fatalln(err)
 	}
 
-	// Convert the body to type string
+	// Return the body as string
 	msg := string(body)
 	return msg
 }
