e95c1278d9e2b6c44946cf603d9b06fb9b8cc55c
cybardev <sheikh@cybar.dev>
2024-03-05 19:30:23 -0400

    test: add stubs for search and resparse tests

---
+++ cmd/ytgo/search_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestGetRequest(t *testing.T) {}
+
+func TestGetSearchResults(t *testing.T) {}
+
+func TestGetVideoFromSearch(t *testing.T) {}
+
+func TestGetVideoFromURL(t *testing.T) {}
